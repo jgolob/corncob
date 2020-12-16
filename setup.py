8bcbf1937826aea5dafe545637b7f41824bd1b6b
Jonathan Golob <j-dev@golob.org>
2020-12-16 11:08:03 -0500

    Basic details added

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="corncob",
+    version="0.0.1",
+    author="Jonathan Golob",
+    author_email="j-dev@golob.org",
+    description="beta-binomial based testing of count data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jgolob/corncob",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
